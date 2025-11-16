0173ddd408d033d237cf28fb4f1ca9e7dca8be56
maciej <mcj.zychlinski@gmail.com>
2025-11-16 20:35:26 +0100

    feat: Add Maja Mug product

--- store/models.py
+++ store/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='item_images/', blank=True, null=True)
     
     def __str__(self):
         return self.name
